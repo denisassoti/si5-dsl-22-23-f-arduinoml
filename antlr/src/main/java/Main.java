95b226d70d943b9c7c759e8c68b6f940f7621f2f
Lidaou Denis Assoti <denisassoti@yahoo.com>
2022-11-30 14:36:08 +0100

    antlr works

--- antlr/src/main/java/Main.java
+++ antlr/src/main/java/Main.java
@@ -1,6 +1,8 @@
 import io.github.mosser.arduinoml.externals.antlr.ModelBuilder;
 import io.github.mosser.arduinoml.externals.antlr.StopErrorListener;
 import io.github.mosser.arduinoml.externals.antlr.grammar.*;
+import io.github.mosser.arduinoml.externals.antlr.grammar.ArduinomlLexer;
+import io.github.mosser.arduinoml.externals.antlr.grammar.ArduinomlParser;
 import io.github.mosser.arduinoml.kernel.App;
 import io.github.mosser.arduinoml.kernel.generator.ToWiring;
 import io.github.mosser.arduinoml.kernel.generator.Visitor;
@@ -37,7 +39,7 @@ public class Main {
         lexer.removeErrorListeners();
         lexer.addErrorListener(new StopErrorListener());
 
-        ArduinomlParser   parser  = new ArduinomlParser(new CommonTokenStream(lexer));
+        ArduinomlParser parser  = new ArduinomlParser(new CommonTokenStream(lexer));
         parser.removeErrorListeners();
         parser.addErrorListener(new StopErrorListener());
 
